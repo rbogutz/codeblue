1f4ba90eadf19befa2c8efc8c54898287a5adee0
rbogutz <bogutzr1@tcnj.edu>
2020-02-24 17:46:20 -0500

    add home page for stub

---
+++ src/app/controllers/static_pages_controller.rb
@@ -0,0 +1,9 @@
+# Project name: Code Blue
+# Description: This project assists the homeless during cold weather
+# Filename: static_pages_controller.rb 
+# Description: controller for static pages generator, used for homepage
+# Last modified on: 24 Feb 2020
+class StaticPagesController < ApplicationController
+def home
+end
+end
