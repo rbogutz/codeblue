13be5dcb7394c04e5e6fc86dfa63bdf23e62101f
rbogutz <bogutzr1@tcnj.edu>
2020-03-29 22:13:51 -0400

    add new report page

--- src/app/controllers/static_pages_controller.rb
+++ src/app/controllers/static_pages_controller.rb
@@ -2,8 +2,8 @@
 # Description: This project assists the homeless during cold weather
 # Filename: static_pages_controller.rb 
 # Description: controller for static pages generator, used for homepage
-# Last modified on: 24 Feb 2020
+# Last modified on: 29 March 2020
 class StaticPagesController < ApplicationController
-def home
-end
+    def home
+    end
 end
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -2,9 +2,12 @@
 # Description: This project assists the homeless during cold weather
 # Filename: routes.rb 
 # Description: Sets routes for pages
-# Last modified on: 24 Feb 2020
+# Last modified on: 29 March 2020
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 root 'static_pages#home' # Set route to home page
+
+resources:reports
+
 end
