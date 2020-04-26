1291214e4b3ec9fb588d766b2dc7fd955974b20a
rbogutz <bogutzr1@tcnj.edu>
2020-04-26 14:03:58 -0400

    add homepage

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -4,6 +4,7 @@
 # Description: Sets routes for pages
 # Last modified on: 29 March 2020
 Rails.application.routes.draw do
+  resources :reports
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 root 'static_pages#home' # Set route to home page
