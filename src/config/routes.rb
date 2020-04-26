8c06fe650c33cec21229e0b0ed9647c2b2451f04
rbogutz <bogutzr1@tcnj.edu>
2020-04-26 17:19:01 -0400

    add weather and location models, more view changes

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -4,6 +4,8 @@
 # Description: Sets routes for pages
 # Last modified on: 29 March 2020
 Rails.application.routes.draw do
+  resources :weathers
+  resources :locations
   resources :reports
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
